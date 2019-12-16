222d23c6329c64b73f84d550863ad8689153b4f2
2403102119 <m17600167028@163.com>
2019-12-16 14:14:51 +0800

    Initial commit

---
+++ app/src/main/java/com/lxkj/hrhc/Bean/CityModel.java
@@ -0,0 +1,12 @@
+package com.lxkj.hrhc.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * kylin on 16/9/24.
+ */
+
+public class CityModel {
+    public List<City> cities=new ArrayList<>();
+}
---
+++ app/src/main/java/com/lxkj/hrhc/wxapi/WXEntryActivity.java
@@ -0,0 +1,10 @@
+package com.lxkj.hrhc.wxapi;
+
+//暂时不用
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+public class WXEntryActivity extends WXCallbackActivity {
+
+
+}
